fe490b8d5759ab694dea2ce777714dd6da68da78
Niklas Köhn <n.koehn@esri.de>
2023-05-23 11:34:02 +0200

    geoJson: adding FLs

--- geoJsonBuildingFootprints/src/main.js
+++ geoJsonBuildingFootprints/src/main.js
@@ -1,5 +1,6 @@
 import Map from "@arcgis/core/Map.js";
 import GeoJSONLayer from "@arcgis/core/layers/GeoJSONLayer.js";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer.js";
 import MapView from "@arcgis/core/views/MapView.js";
 import SceneView from "@arcgis/core/views/SceneView.js";
 import "./style.css";
@@ -13,6 +14,9 @@ const template = {
   title: "Building Footprints",
   content: "The height of this {type} building is {height}."
 };
+
+let map2d = undefined;
+let map3d = undefined;
 
 
 const create2dMap = async () => {
@@ -71,7 +75,7 @@ const create2dMap = async () => {
   gQuery.where = '1=1';
   const extent = await geojsonLayer.queryExtent(gQuery);
 
-  const map2d = new Map({
+  map2d = new Map({
     basemap: "gray-vector",
     layers: [geojsonLayer]
   });
@@ -145,7 +149,7 @@ const create3dScene = async () => {
   gQuery.where = '1=1';
   const extent = await geojsonLayer3D.queryExtent(gQuery);
   
-  const map3d = new Map({
+  map3d = new Map({
     basemap: "gray-vector",
     layers: [geojsonLayer3D]
   });
@@ -163,6 +167,18 @@ const create3dScene = async () => {
   });
 } 
 
-create2dMap();
 
-create3dScene();
+const addFeatureLayer = () => {
+  const flUrl = "https://services2.arcgis.com/jUpNdisbWqRpMo35/ArcGIS/rest/services/Berliner_Mauer/FeatureServer/3";
+  const fl2d = new FeatureLayer({url: flUrl, id: 'Berlin random FL 2D'});
+  map2d.add(fl2d);
+  const fl3d = new FeatureLayer({url: flUrl, id: 'Berlin random FL 3D'});
+  map3d.add(fl3d);
+  console.log('adding FLs', fl2d, fl3d, map2d, map3d);
+}
+
+const handle2d = create2dMap();
+const handle3d = create3dScene();
+
+await Promise.all([handle2d, handle3d]);
+addFeatureLayer();
