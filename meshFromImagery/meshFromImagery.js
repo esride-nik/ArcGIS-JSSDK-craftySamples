3bd718b2361a51bf3eaaadc41a09542e08db859a
Niklas Köhn <n.koehn@esri.de>
2023-12-12 18:10:47 +0100

    yup

--- meshFromImagery/meshFromImagery.js
+++ meshFromImagery/meshFromImagery.js
@@ -8,12 +8,52 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+const GraphicsLayer_1 = __importDefault(require("@arcgis/core/layers/GraphicsLayer"));
+const ImageryLayer_1 = __importDefault(require("@arcgis/core/layers/ImageryLayer"));
 let view;
 (_a = document === null || document === void 0 ? void 0 : document.querySelector("arcgis-scene")) === null || _a === void 0 ? void 0 : _a.addEventListener("viewReady", (event) => __awaiter(void 0, void 0, void 0, function* () {
     view = event.detail.view;
     runApp();
 }));
+let imageryLayer;
+let volumetricGraphicsLayer;
+const setupView = () => {
+    const clippingAdd = 100;
+    const extentAdd = 1000;
+    view.viewingMode = "local";
+    view.clippingArea = {
+        spatialReference: { latestWkid: 3857, wkid: 102100 },
+        xmin: -4891786.441670591 - clippingAdd,
+        ymin: -2307257.926811594 - clippingAdd,
+        xmax: -4891427.934010591 + clippingAdd,
+        ymax: -2306963.3097615945 + clippingAdd,
+    };
+    view.extent = {
+        spatialReference: { latestWkid: 3857, wkid: 102100 },
+        xmin: -4891786.441670591 - extentAdd,
+        ymin: -2307257.926811594,
+        xmax: -4891427.934010591 + extentAdd,
+        ymax: -2306963.3097615945 + extentAdd * 2,
+    };
+};
+const addLayers = () => {
+    imageryLayer = new ImageryLayer_1.default({
+        id: 'Elevation in bands',
+        url: 'https://tiledimageservices.arcgis.com/OLiydejKCZTGhvWg/arcgis/rest/services/VarzeaMin_DOMs/ImageServer',
+        opacity: 0.9
+    });
+    volumetricGraphicsLayer = new GraphicsLayer_1.default({
+        id: 'volumetricGraphics'
+    });
+    view.map.addMany([imageryLayer, volumetricGraphicsLayer]);
+};
 const runApp = () => {
     console.log('View \'s ready. Continue imperatively from here.', view);
+    setupView();
+    addLayers();
 };
--- meshFromImagery/meshFromImagery.ts
+++ meshFromImagery/meshFromImagery.ts
@@ -1,4 +1,6 @@
 import SceneView from "@arcgis/core/views/SceneView";
+import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
+import ImageryLayer from '@arcgis/core/layers/ImageryLayer';
 
 let view: SceneView;
 document?.querySelector("arcgis-scene")?.addEventListener("viewReady", async (event: any) => {
@@ -6,6 +8,47 @@ document?.querySelector("arcgis-scene")?.addEventListener("viewReady", async (ev
     runApp();
 });
 
+let imageryLayer: ImageryLayer;
+let volumetricGraphicsLayer: GraphicsLayer;
+
+const setupView = () => {
+
+    const clippingAdd = 100;
+    const extentAdd = 1000;
+    view.viewingMode = "local";
+    view.clippingArea = {
+        spatialReference: { latestWkid: 3857, wkid: 102100 } as unknown as __esri.SpatialReference,
+        xmin: -4891786.441670591 - clippingAdd,
+        ymin: -2307257.926811594 - clippingAdd,
+        xmax: -4891427.934010591 + clippingAdd,
+        ymax: -2306963.3097615945 + clippingAdd,
+      } as unknown as __esri.Extent;
+
+    view.extent = {
+        spatialReference: { latestWkid: 3857, wkid: 102100 } as unknown as __esri.SpatialReference,
+        xmin: -4_891_786.441_670_591 - extentAdd,
+        ymin: -2_307_257.926_811_594,
+        xmax: -4_891_427.934_010_591 + extentAdd,
+        ymax: -2_306_963.309_761_594_5 + extentAdd * 2,
+      } as unknown as __esri.Extent;
+
+}
+      
+const addLayers = () => {
+    imageryLayer = new ImageryLayer({
+        id: 'Elevation in bands',
+        url: 'https://tiledimageservices.arcgis.com/OLiydejKCZTGhvWg/arcgis/rest/services/VarzeaMin_DOMs/ImageServer',
+        opacity: 0.9
+    });
+
+    volumetricGraphicsLayer = new GraphicsLayer({
+        id: 'volumetricGraphics'
+    })
+    view.map.addMany([imageryLayer, volumetricGraphicsLayer]);
+ }
+
 const runApp = () => {
     console.log('View \'s ready. Continue imperatively from here.', view);
+    setupView();
+    addLayers();
 }
